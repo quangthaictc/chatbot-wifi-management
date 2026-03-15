1f773ac98842e15c5beb19c36ae044b920c91047
quangthaictc <quangthaictc@gmail.com>
2026-03-15 03:59:15 +0000

    feat: basic setup topology mininet-wifi

--- topology/simplewifitopology.py
+++ topology/simplewifitopology.py
@@ -1,14 +1,13 @@
 #!/usr/bin/env python
 
-"This example creates a simple network topology with 1 AP and 2 stations"
+'This example creates a simple network topology with 1 AP and 2 stations'
 
 import sys
-
-from ryu import controller
 
 from mininet.log import setLogLevel, info
 from mn_wifi.cli import CLI
 from mn_wifi.net import Mininet_wifi
+from mininet.node import RemoteController
 
 
 def topology():
@@ -17,19 +16,27 @@ def topology():
 
     info("*** Creating nodes\n")
     sta_arg, ap_arg = {}, {}
-    if "-v" in sys.argv:
-        sta_arg = {"nvif": 2}
+    if '-v' in sys.argv:
+        sta_arg = {'nvif': 2}
     else:
         # isolate_clientes: Client isolation can be used to prevent low-level
         # bridging of frames between associated stations in the BSS.
         # By default, this bridging is allowed.
         # OpenFlow rules are required to allow communication among nodes
-        ap_arg = {"client_isolation": True}
+        ap_arg = {'client_isolation': False}
 
-    ap1 = net.addAccessPoint("ap1", ssid="simpletopo", mode="g", channel="5", **ap_arg)
-    sta1 = net.addStation("sta1", **sta_arg)
-    sta2 = net.addStation("sta2")
-    c0 = net.addController("c0", controller=RemoteController, ip="127.0.0.1", port=6633)
+    ap1 = net.addAccessPoint('ap1', ssid="HUFLIT", mode="g",
+                             channel="5", **ap_arg)
+    
+    ap2 = net.addAccessPoint('ap2', ssid="HUFLIT-GV", mode="g",
+                             channel="5", **ap_arg)
+
+    sta1 = net.addStation('sta1')
+    sta2 = net.addStation('sta2')
+    sta3 = net.addStation('sta3')
+    sta4 = net.addStation('sta4') 
+    
+    c0 = net.addController('c0', controller=RemoteController, ip="127.0.0.1", port=6633)
 
     info("*** Configuring nodes\n")
     net.configureNodes()
@@ -37,6 +44,8 @@ def topology():
     info("*** Associating Stations\n")
     net.addLink(sta1, ap1)
     net.addLink(sta2, ap1)
+    net.addLink(sta3, ap2)
+    net.addLink(sta4, ap2)
 
     info("*** Starting network\n")
     net.build()
@@ -49,7 +58,6 @@ def topology():
     info("*** Stopping network\n")
     net.stop()
 
-
-if __name__ == "__main__":
-    setLogLevel("info")
+if __name__ == '__main__':
+    setLogLevel('info')
     topology()
